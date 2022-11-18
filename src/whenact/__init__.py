50f82819fbfc5255ffe543cacfec9b519849ad05
morvanzhou <morvanzhou@hotmail.com>
2022-11-18 15:29:19 +0800

    init

--- src/whenact/__init__.py
+++ src/whenact/__init__.py
@@ -1,4 +1,4 @@
-from whenact.block.when_block import when
 from whenact.block.act_block import act
-from whenact.pipeline import Pipeline, create_pipeline, Policy
+from whenact.block.when_block import when
 from whenact.context import PipelineContext, BaseContext
+from whenact.pipeline import Pipeline, create_pipeline, Policy
